af8f07aabfdca40c7fc3f6be44b3edff9f00b556
Jose Castagnino <jose@muttdata.ai>
2024-01-09 22:53:20 -0300

    add as_numpy method

--- src/reinforcement_lenin/grid.py
+++ src/reinforcement_lenin/grid.py
@@ -1,21 +1,22 @@
 import networkx as nx
+import numpy as np
 
 
 class Board:
     def __init__(self, N: int = 4):
-        BOARD_SIZE = N**2
+        self.board_size = N**2
         self.board: dict = {0: dict()}
-        for k in range(1, BOARD_SIZE - 1):
+        for k in range(1, self.board_size - 1):
             self.board[k] = {'up': k - N, 'down': k + N, 'left': k - 1, 'right': k + 1}
             if k - N < 0:
                 self.board[k]['up'] = k
-            if k + N > BOARD_SIZE - 1:
+            if k + N > self.board_size - 1:
                 self.board[k]['down'] = k
             if k % N == 0:
                 self.board[k]['left'] = k
             if k % N == N - 1:
                 self.board[k]['right'] = k
-        self.board[BOARD_SIZE - 1] = dict()
+        self.board[self.board_size - 1] = dict()
 
         self.graph = nx.DiGraph()
         for k, n in self.board.items():
@@ -29,6 +30,10 @@ class Board:
             return 1
         else:
             return 0
+
+    def as_numpy(self):
+        nodes = np.array(self.graph.nodes)
+        return np.reshape(nodes, (self.board_size, self.board_size))
 
     def plot_graph(self):
         nx.draw(self.graph, with_labels=True)
